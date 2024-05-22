8d0599ded75d92f103b76ba6761c4cf8f06c1c3c
GustavOxys <gustavohzmdev@gmail.com>
2024-05-22 11:43:31 -0300

    Filtra e ordena posts atraves do context das views nos templates

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,11 +1,15 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from blog.models import Post
 
-posts = list(range(1000))
+PER_PAGE = 9
 
 
 def index(request):
-    paginator = Paginator(posts, 9)
+    posts = Post.objects.filter(is_published=True).order_by('-pk')
+    
+
+    paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
@@ -18,10 +22,7 @@ def index(request):
     )
 
 
-def page(request):
-    paginator = Paginator(posts, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+def page(request):    
 
     return render(
         request,        
@@ -33,9 +34,7 @@ def page(request):
 
 
 def post(request):
-    paginator = Paginator(posts, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+    
 
     return render(
         request,        
